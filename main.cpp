519e31e3a76b2f9650e2fb3b1f46642b3e5e0ed0
Jedrick Espiritu <Jed.Espiritu.000@gmail.com>
2025-05-11 09:19:07 -0700

    created weights & bias file verification

--- main.cpp
+++ main.cpp
@@ -6,35 +6,60 @@
 
 	**KEY THINGS TO NOTE WHEN INTERACTING WITH THIS PROGRAM**
 
-	* when you want to generate a new neural network, delete the "weights.csv" file
-	* this program is hard-written with a single neuron for output (meaning that you should only be predicting one value)
-	* in the csv file that you will parse, make sure that the last column is the column that you want to predict (i.e., if
-	  you want to predict house prices, then every other column will be the house features, and then the last column will
-	  be the actual house prices)
-	* the program uses stochastic gradient descent as I do not have the appropriate skillset to apply parallelization as of yet
+	- This program is hard-written with a single neuron for output (meaning that you should only be predicting one value)
+
+	- In the csv dataset file that you will parse, make sure...
+		
+		1. That the last column is the column that you want to predict (i.e., if you want to predict house prices, then 
+		every column except the last will be the house features, and then the last column will be the actual house prices)
+		
+		2. ***** THAT EVERY FIELD ONLY CONSISTS OF NUMBER VALUES *****; do not have strings/words/letters in the dataset 
+		(i.e., do not have a column of strings like "name of house owner" and have every field underneath be strings like 
+		"Jack Fry" or "Jill" or "ioenhaoea")
+
 */
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <cmath>
 #include <random>
+#include <string>
+#include <cctype>
 #include "NeuralNetwork.h"
 #include "DenseLayer.h"
 #include "Neuron.h"
 
-void generate_weight_file(std::fstream&, int number_of_neurons_each_layer[], int number_of_layers, int number_of_features);
-void parse_csv();
-void normalize_features();
+void generate_weights_and_biases_file(const std::string& weights_and_biases_file_name, const int* number_of_neurons_each_hidden_layer,
+	const int& number_of_hidden_layers, const int& number_of_features);
+void generate_weights_and_biases_for_layer(std::fstream& weights_and_biases_file, int number_of_features, int number_of_neurons);
+void parse_csv_sample_data(const std::string& dataset_name, double** training_samples, double* target_values, 
+	const int& number_of_features, const int& number_of_samples);
+void calculate_standard_deviations(double* standard_deviations, const int& number_of_features);
+void calculate_means(double* means, const int& number_of_features);
+void normalize_input_features();
+bool verify_weights_and_biases_file(std::fstream& weights_and_biases_file, int* number_of_neurons_each_hidden_layer, 
+	int number_of_hidden_layers, const int& number_of_features);
+
+// generate a random seed for random numbers
+std::random_device rd;
+std::mt19937 gen(rd());
 
 // driver for class
 int main()
 {
-	// !!! NOTE that the last column is the column of values you want to predict !!!
-	// for example, if you have features of a house and want to predict price, then make the house prices the last column
-	// of the csv that you want to use as your database
+	// !!! REMINDER that the last column is the column of values you want to predict !!!
+		// for example, if you have features of a house and want to predict price, then make the house prices the last column
+		// of the csv that you want to use as your database
+	// !!! ALSO !!! the target values column does not count towards the number of features (i.e., if you have only 2 features 
+	// and are predicting house prices, just put number_of_features as 2)
+
+
 	// data set used: https://www.kaggle.com/datasets/meharshanali/amazon-stocks-2025	
-	int number_of_samples = 6988;
-	int number_of_features = 6;
+	// order of columns: Date, Open, High, Low, Volume, Close
+	int number_of_samples = 6987;
+	int number_of_features = 5;
+
 
 	// !!! NOTE AGAIN that this program is hard-written with a single neuron for output !!!
 	// this is the order and number of neurons you want in each hidden layer
@@ -42,74 +67,207 @@ int main()
 		// the first hidden layer will have 5 neurons
 		// the second hidden layer will have 10 neurons
 		// the third hidden layer will have 3 neurons
-	int number_of_neurons_each_layer[] = { 5, 10, 3 };
+	int number_of_neurons_each_hidden_layer[] = { 7, 10, 3 };
+
 
 	// change this value to change how "fast" the neural network learns your data
 	double learning_rate = 0.01;
 
+
 	// change this value to regularize and prevent overfitting for the neural network
 	double regularization = 0.01;
 
+	// PAST THIS POINT IS ALL THE HARD CODE; REFER TO ABOVE PARTS FOR EDITABLE COMPONENTS
+
+	double* standard_deviations = new double[number_of_features];
+	double* means = new double[number_of_features];
+
+	double** training_samples = new double* [number_of_samples];
+
+	// allocate memory to each array stored in the training samples
+	for (int i = 0; i < number_of_samples; i++)
+		training_samples[i] = new double[number_of_features];
+	
+	double* target_values = new double[number_of_samples];
+
+	// parse the csv dataset into the 2d training samples array
+	std::string dataset_name = "./dataset.csv";
+	parse_csv_sample_data(dataset_name, training_samples, target_values, number_of_features, number_of_samples);
+
+
 	// open the file for weights
-	std::fstream weight_file("./weights.csv", std::ios::out | std::ios::in);
+	std::string weights_and_biases_file_name = "./weights_and_biases.csv";
+	std::fstream weights_and_biases_file(weights_and_biases_file_name, std::ios::out | std::ios::in);
+
 	// if the weight file was not opened (therefore doesn't exist), initialize a new one using He initialization
-	if (!weight_file)
+	if (!weights_and_biases_file)
 	{
-		generate_weight_file(weight_file, number_of_neurons_each_layer, 
-			sizeof(number_of_neurons_each_layer) / sizeof(int), number_of_features);
-		weight_file.open("./weights.csv", std::ios::out | std::ios::in);
+		std::cout << "Weights and biases file not found; creating new one...";
+		
+		// create a new weight file
+		generate_weights_and_biases_file(weights_and_biases_file_name, number_of_neurons_each_hidden_layer, 
+			sizeof(number_of_neurons_each_hidden_layer) / sizeof(int), number_of_features);
+
+		weights_and_biases_file.open(weights_and_biases_file_name, std::ios::out | std::ios::in);
 	}
+
+	// verify the weights and biases file is valid
+	if (!verify_weights_and_biases_file(weights_and_biases_file, number_of_neurons_each_hidden_layer, sizeof(number_of_neurons_each_hidden_layer) / sizeof(int), number_of_features))
+	{
+		char option;
+
+		std::cout << "\nWeights and biases file is erroneous (there are not enough weights and biases to accomodate all features)"
+			<< "\nWould you like to generate a new weights and biases file? (WARNING: this is effective "
+			<< "\nto resetting your neural network, and should only be done when changing the number of features "
+			<< "or changing the number of neurons in the hidden layers) (Y/N): ";
+		std::cin >> option;
+
+		while (tolower(option) != 'y' && tolower(option) != 'n')
+		{
+			std::cout << "[ERROR] Invalid input. Please enter only yes or no (Y/N): ";
+			std::cin >> option;
+		}
+
+		if (tolower(option) == 'y')
+		{
+			weights_and_biases_file.close();
+
+			generate_weights_and_biases_file(weights_and_biases_file_name, number_of_neurons_each_hidden_layer,
+				sizeof(number_of_neurons_each_hidden_layer) / sizeof(int), number_of_features);
+
+			weights_and_biases_file.open(weights_and_biases_file_name, std::ios::out | std::ios::in);
+		}
+	}
+	
 }
 
-// generate weight file if not already made
-void generate_weight_file(std::fstream& weight_file, int number_of_neurons_each_layer[], int number_of_layers, int number_of_features)
+// verify the file has the correct number of weights and bias values
+bool verify_weights_and_biases_file(std::fstream& weights_and_biases_file, int* number_of_neurons_each_hidden_layer, int number_of_hidden_layers,
+	const int& number_of_features)
 {
-	// create a new weight file
-	weight_file.open("./weights.csv", std::ios::out);
+	// count the number of weights and biases in a given row
+	// for a given row, there should be a number of features + one to simulate
+	// how each neuron will have an equal number of weights + one bias value
+	int field_count = 0;
 
-	// generate a random seed for random numbers
-	std::random_device rd;
-	std::mt19937 gen(rd());
+	std::string line;
+	getline(weights_and_biases_file, line);
 
+	std::stringstream ss(line);
+	std::string value;
+
+	if (field_count != number_of_features + 1) return false;
+
+	for (int l = 0; l < number_of_hidden_layers; l++)
+	{
+		// reset the field count for the line
+		field_count = 0;
+
+		// continue to the next layer's neurons
+		for (int i = 0; i < number_of_neurons_each_hidden_layer[l] - 1; i++)
+			getline(weights_and_biases_file, line);
+
+		getline(weights_and_biases_file, line);
+		ss.str(line);
+
+		for (int f = 0; f < number_of_features; f++)
+		{
+			getline(ss, value, ',');
+			field_count++;
+		}
+
+		if (field_count != number_of_neurons_each_hidden_layer[l] + 1) return false;
+	}
+
+	weights_and_biases_file.seekg(0); // reset to start
+	return true;
+}
+
+
+
+// generate weight file if not already made
+void generate_weights_and_biases_file(const std::string& weights_and_biases_file_name,  const int* number_of_neurons_each_hidden_layer, 
+	const int& number_of_hidden_layers, const int& number_of_features)
+{
+	// create the file
+	std::fstream weights_and_biases_file(weights_and_biases_file_name, std::ios::out | std::ios::trunc);
+
+	generate_weights_and_biases_for_layer(weights_and_biases_file, number_of_features, number_of_neurons_each_hidden_layer[0]);
+
+	// now generate initial weights using He initialization for every subsequent layer and store in weight file
+	for (int l = 1; l < number_of_hidden_layers; l++)
+		generate_weights_and_biases_for_layer(weights_and_biases_file, number_of_neurons_each_hidden_layer[l - 1], 
+			number_of_neurons_each_hidden_layer[l]);
+
+	generate_weights_and_biases_for_layer(weights_and_biases_file, number_of_neurons_each_hidden_layer[number_of_hidden_layers - 1], 1);
+
+	// close the file
+	weights_and_biases_file.close();
+}
+
+void generate_weights_and_biases_for_layer(std::fstream& weights_and_biases_file, int number_of_features, int number_of_neurons)
+{
 	// use He initialization for the weights provided the number of input features into the first layer
 	double stddev = sqrt(2.0 / number_of_features);
 	std::normal_distribution<double> dist(0.0, stddev);
 	// for each neuron in the first layer
-	for (int n = 0; n < number_of_neurons_each_layer[0]; n++)
+	for (int n = 0; n < number_of_neurons; n++)
 	{
 		// insert an equal number of weights into the 
 		for (int w = 0; w < number_of_features; w++)
-			weight_file << dist(gen) << ",";
+			weights_and_biases_file << dist(gen) << ",";
 
 		// insert an initial bias value of 0 and then end the current neuron line
-		weight_file << 0 << '\n';
+		weights_and_biases_file << 0 << '\n';
 	}
-
-	// now generate initial weights using He initialization for every subsequent layer and store in weight file
-	for (int l = 1; l < number_of_layers; l++)
-	{
-		// create a random number generator with He initialization
-		// the number of features for a given layer l is the l - 1 number of neurons because these are dense layers
-		stddev = sqrt(2.0 / number_of_neurons_each_layer[l - 1]);
-		dist.param(std::normal_distribution<double>::param_type(0.0, stddev));
-
-		// for each neuron in current the layer, initialize weights for each neuron
-		for (int n = 0; n < number_of_neurons_each_layer[l]; n++)
-		{
-			for (int w = 0; w < number_of_neurons_each_layer[l - 1]; w++)
-				weight_file << dist(gen) << ",";
-
-			// insert an initial bias value of 0 and then end the current neuron line
-			weight_file << 0 << '\n';
-		}
-	}
-
-	// close the new file so it can be reopened with new settings
-	weight_file.close();
 }
 
-// parse in a csv of data
-void parse_csv()
+// parse in the csv of data
+void parse_csv_sample_data(const std::string& dataset_name, double** training_samples, double* target_values, 
+	const int& number_of_features, const int& number_of_samples)
+{
+	std::ifstream dataset_file(dataset_name, std::ios::in);
+
+	if (!dataset_file.is_open()) {
+		std::cerr << "[ERROR] The dataset could not be found within the project; please edit the \"dataset_name\" variable "
+			<< "to the dataset's name, or otherwise include the dataset within the project." << std::endl;
+	}
+	else
+	{
+
+		std::string line;
+
+		// for each training sample t
+		for (int t = 0; t < number_of_samples; t++)
+		{
+			// get the line of features and target value
+			getline(dataset_file, line);
+
+			std::stringstream ss(line);
+			std::string value;
+
+			for (int f = 0; f < number_of_features; f++)
+			{
+				getline(ss, value, ',');
+				training_samples[t][f] = std::stod(value);
+			}
+			
+			// get the target value, which is the last column
+			getline(ss, value, '\n');
+			target_values[t] = std::stod(value);
+		}
+		dataset_file.close();
+	}
+}
+
+// 
+void calculate_standard_deviations(double* standard_deviations, const int& number_of_features)
+{
+
+}
+
+// calculate the average of each feature
+void calculate_means(double* means, const int& number_of_features)
 {
 
 }
